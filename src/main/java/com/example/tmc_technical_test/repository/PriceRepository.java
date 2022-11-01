068aedc456fc46993c1a500f485cdbbcda62d224
portelle <n>
2022-11-01 19:58:59 +0100

    Removed models from generated sources. Moved the code to the project entities. Fixed problems with the code.

--- src/main/java/com/example/tmc_technical_test/repository/PriceRepository.java
+++ src/main/java/com/example/tmc_technical_test/repository/PriceRepository.java
@@ -4,7 +4,9 @@ import com.example.tmc_technical_test.entities.PriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PriceRepository extends JpaRepository<PriceEntity, Integer> {
-
+    public List<PriceEntity> findByStartDateGreaterThanEqualAndEndDateLessThanEqual(String date, String dateCopy);
 }
